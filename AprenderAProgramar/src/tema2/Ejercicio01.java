060e0100f3dff09c19cb50f477f3760e5cce0370
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-10-20 14:42:25 +0200

    Hecho el 01

--- AprenderAProgramar/src/tema2/Ejercicio01.java
+++ AprenderAProgramar/src/tema2/Ejercicio01.java
@@ -1,7 +1,21 @@
 package tema2;
 
+import java.util.Scanner;
+
 public class Ejercicio01 {
 	public static void main(String[] args) {
-
+		int num1, num2, num3;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca un número");
+		num1 = sc.nextInt();
+		System.out.println("Introduzca otro número");
+		num2 = sc.nextInt();
+		System.out.println("Introduzca otro número");
+		num3 = sc.nextInt();
+		if (num1 > 10 || num2 > 10 || num3 > 10)
+			System.out.println("ALGUNO MAYOR QUE 10");
+		else
+			System.out.println("NINGUNO MAYOR QUE 10");
+		sc.close();
 	}
 }
