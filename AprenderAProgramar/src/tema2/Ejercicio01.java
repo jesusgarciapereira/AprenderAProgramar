226bac044bfef49ffa063ba6c471282139a4f8d9
Jesús <Jess@LAPTOP-8MILMTUM>
2023-10-21 15:33:40 +0200

    01 terminado y comentado

--- AprenderAProgramar/src/tema2/Ejercicio01.java
+++ AprenderAProgramar/src/tema2/Ejercicio01.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 
 public class Ejercicio01 {
 	public static void main(String[] args) {
+		// Declaramos las variables con los tres números que nos piden
 		int num1, num2, num3;
+		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		// Le pedimos al usuario el primer número
 		System.out.println("Introduzca un número");
+		// Leemos el número
 		num1 = sc.nextInt();
+		// Le pedimos al usuario el segundo número
 		System.out.println("Introduzca otro número");
+		// Leemos el número
 		num2 = sc.nextInt();
+		// Le pedimos al usuario el tercer número
 		System.out.println("Introduzca otro número");
+		// Leemos el número
 		num3 = sc.nextInt();
+		// Si, al menos, uno de los números es mayor que 10 mostrará este mensaje
 		if (num1 > 10 || num2 > 10 || num3 > 10)
-			System.out.println("ALGUNO MAYOR QUE 10");
+			System.out.println("ALGUNO MAYOR QUE 10"); 
+		// En caso contrario mostrará que ninguno es mayor que 10
 		else
-			System.out.println("NINGUNO MAYOR QUE 10");
+			System.out.println("NINGUNO MAYOR QUE 10"); 
+		// Cerramos el Scanner
 		sc.close();
 	}
 }
