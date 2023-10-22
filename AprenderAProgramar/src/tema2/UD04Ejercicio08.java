7ea44f1677e94dedab0ad09ac77a6e1bc6f0dcab
Jesús <Jess@LAPTOP-8MILMTUM>
2023-10-22 12:29:54 +0200

    UD04 08 Hecho y comentado

---
+++ AprenderAProgramar/src/tema2/UD04Ejercicio08.java
@@ -0,0 +1,28 @@
+package tema2;
+
+import java.util.Scanner;
+
+public class UD04Ejercicio08 {
+
+	public static void main(String[] args) {
+		// Declaramos la variable edad que pediremos al usuario
+		int edad;
+		// Activamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		// Presentamos el programa
+		System.out.println("¿Eres mayor de edad?");
+		// Le pedimos al usuario que escriba la edad
+		System.out.println("Introduce tu edad; si eres mayor de edad se lo mostraré, si no, no mostraré nada");
+		// Leemos el número
+		edad = sc.nextInt();
+		/*
+		 * Si la edad es mayor o igual que 18, mostrará esta instrucción; si no, no
+		 * mostrará nada
+		 */
+		if (edad >= 18)
+			System.out.println("Eres mayor de edad");
+		// Cerramos el Scanner
+		sc.close();
+	}
+
+}
